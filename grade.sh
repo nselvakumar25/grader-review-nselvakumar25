a6156dfa1480871a2fd01cab1470c6fc18bc67eb
nselvakumar25 <nselvakumar@ucsd.edu>
2023-11-07 17:23:32 -0800

    ...

--- grade.sh
+++ grade.sh
@@ -20,15 +20,16 @@ fi
 
 cd ..
 cp -r student-submission/ListExamples.java grading-area
+cp -r lib grading-area
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
-
+cd grading-area
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples.java > grep-results.txt
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grep-results.txt
 
 if grep "FAILURES!!!" grep-results.txt
 then 
